1784e03f9b6c445482002dcfc5ca27fe7344061f
G228470 <228470@st.takushoku-u.ac.jp>
2023-07-06 16:42:54 +0900

    a

--- kazuate.js
+++ kazuate.js
@@ -3,56 +3,54 @@ let kotae = Math.floor(Math.random()*10) + 1;
 console.log('答え（デバッグ用）: ' + kotae);
 
 // 入力回数（予想回数）
-let kaisu = 0;
+let kaisu = 1;
+
 // 予想を4回実行する
 // 将来以下の hantei(); の4回の呼び出しを全て削除する
-let b = document.querySelector('#print');
+// 代わりにここでは，ボタンを押したら hantei() を呼び出すイベント処理をする
+let b = document.querySelector( '#answer');
 b.addEventListener('click', hantei);
-
-
 
 // ボタンを押した後の処理をする関数 hantei() の定義
 function hantei() {
+  // 将来ここでは 4 ではなくテキストボックスに指定された数値を yoso に代入する
+  
+  // 課題3-1: 正解判定する  
   let i = document.querySelector('input[name="yoso"]');
-  let yoso=i.value;
-  kaisu+=1;
-  let k1 = document.querySelector('span#kaisu');
-  k1.textContent = kaisu; 
-  let k2 = document.querySelector('span#answer');
-  k2.textContent = yoso;
-  console.log(kaisu+'回目の予想:' +yoso);
-  let p1 = document.querySelector('p#result');
-  if (kaisu >= 4) {
-    p1.textContent = ("答えは" + kotae +"でした．すでにゲームは終わっています");
-    console.log("答えは" + kotae +"でした．すでにゲームは終わっています");
-  }else if(kaisu==3 && kotae!=yoso){
-    p1.textContent = ("まちがい.残念でした答えは" + kotae +"です．"); 
-    console.log("まちがい.残念でした答えは" + kotae +"です．");
-  }else if(kaisu==3 && kotae==yoso ){
-    p1.textContent = ("正解です.おめでとう!");
-    console.log("正解です.おめでとう!");
-  
+  let yoso = i.value; 
 
-  }
-  else{
-    if (yoso > kotae) {
-      p1.textContent = ("まちがい.答えはもっと小さいですよ");
-        console.log("まちがい.答えはもっと小さいですよ");
-      }
-    
-    else if(yoso < kotae) {
-      p1.textContent = ("まちがい.答えはもっと大きいですよ");
-        console.log("まちがい.答えはもっと大きいですよ");
-      }
-    
-    else{
-      p1.textContent = ("正解です.おめでとう!");
-        console.log("正解です.おめでとう!");
-      }
-  }
+  let SpanKaisu = document.querySelector('span#kaisu');
+  SpanKaisu.textContent = kaisu;
 
-  
-  // 課題3-1: 正解判定する
+  let SpanAnswer = document.querySelector('span#answer');
+  SpanAnswer.textContent = yoso;
+
+  let SpanResult = document.querySelector('p#result');
+
+  if(kaisu < 3){
+    if(hantei === kotae){
+      SpanResult.textContent = '正解です。おめでとう!';
+    }else{
+      if(hantei<kotae){
+        SpanResult.textContent = '残念、答えはもっと大きいです。';
+      }else{
+        SpanResult.textContent = '残念、答えはもっと小さいです。';
+      }
+    }
+  }else if(kaisu === 3){
+    if(hantei === kotae){
+      SpanResult.textContent = '正解です。おめでとう!';
+    }else{
+      if(hantei<kotae){
+        SpanResult.textContent = '残念! 答えは'+kotae+'でした';
+      }else{
+        SpanResult.textContent = '残念! 答えは'+kotae+'でした';
+      }
+    }
+    }else if(kaisu > 3){
+      SpanResult.textContent = 'すでにゲームは終わっています。答えは'+kotae+'でした';
+    }
+    kaisu = kaisu + 1;
   // kotae と yoso が一致するかどうか調べて結果を出力
   // 課題3-1における出力先はコンソール
 }
