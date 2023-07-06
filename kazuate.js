4ad5cb3795a0c511771759d9ccdb663926dcce64
G228470 <228470@st.takushoku-u.ac.jp>
2023-07-06 16:48:14 +0900

    a

--- kazuate.js
+++ kazuate.js
@@ -30,8 +30,8 @@ function hantei() {
   if(kaisu < 3){
     if(hantei === kotae){
       SpanResult.textContent = '正解です。おめでとう!';
-    }else{
-      if(hantei<kotae){
+    }else {
+      if(hantei>kotae){
         SpanResult.textContent = '残念、答えはもっと大きいです。';
       }else{
         SpanResult.textContent = '残念、答えはもっと小さいです。';
