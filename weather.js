6d3905b94f5d07428c8c8c6eaed2c041e0ab8525
G228470 <228470@st.takushoku-u.ac.jp>
2023-07-20 16:37:02 +0900

    課題

--- weather.js
+++ weather.js
@@ -1,41 +1,19 @@
-let b1 = document.querySelector('button#print');
-b1.addEventListener('click', iroiro);
+let searchBtn = document.getElementById('search-btn');
+searchBtn.addEventListener('click', iroiro);
+
 function iroiro() {
-  
-  let i = document.querySelector('input[name="basho"]');
-  let g=i.value;
-  let url = 'https://www.nishita-lab.org/web-contents/jsons/openweather/'+g+'.json';
-  axios.get(url)
-  .then(showResult)   // 通信成功
-  .catch(showError)   // 通信失敗
-  .then(finish);      // 通信の最後の処理
-  
-  
-  
-}
-function showResult(resp){
-  let data = resp.data;
-  if (typeof data === 'string') {
-    data = JSON.parse(data);
+  let cityId = document.getElementById('city-id').value.trim(); // Add trim() to remove any leading/trailing spaces
+  if (cityId === '') {
+    console.log('都市のIDを入力してください'); // Log an error message if the input is empty
+    return;
   }
-  console.log(data);
-  console.log(data.name);
-  console.log(data.main.temp_max);
-  console.log(data.main.temp_min);
-  let p1 = document.querySelector('p#name');
-p1.textContent = ("都市名: "+data.name); 
-let p2 = document.querySelector('p#temp_max');
-p2.textContent = ("最高気温: "+data.main.temp_max+"℃ ");
-let p3 = document.querySelector('p#temp_min');
-p3.textContent = ("最低気温: "+data.main.temp_min+"℃");
-let p4= document.querySelector('p#situ');
-p4.textContent = ("湿度: "+data.main.humidity+"% ");
 
+  let url = `https://www.nishita-lab.org/web-contents/jsons/openweather/${cityId}.json`;
+
+  axios.get(url)
+    .then(showResult)
+    .catch(showError)
+    .then(finish);
 }
 
-function showError(err) {
-  console.log(err);
-}
-function finish() {
-  console.log('Ajax 通信が終わりました');
-}
+// Rest of the showResult, showError, and finish functions remain unchanged
